381b6044e6f52078c3630c8879794fbbd4b0ab55
msathe-tech <msathe@Madhavs-MacBook-Pro.local>
2019-01-30 16:52:27 -0500

    lab3 started

---
+++ lab2/README.md
@@ -0,0 +1,7 @@
+# Lab 2 
+Lab2 shows a monolithic application for ACME Inc.
+The application has simple UI that takes user account information and provides most suitable stock to invest.
+Of course, it is based on highly sophisticated predictive analytics and machine learning algorithms. 
+You get it, right? ;)
+
+The monolith includes UI layer, business logic and the API all together in the same app.
--- lab2/src/main/java/io/cna/acme/monolith/AcmeController.java
+++ lab2/src/main/java/io/cna/acme/monolith/AcmeController.java
@@ -4,7 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class AcmeController {
@@ -12,12 +15,16 @@ public class AcmeController {
 
 
     @GetMapping("/service/greet/{name}")
-    public String sayHello(@PathVariable String name) {
+    @ResponseBody
+    public String sayHello(@PathVariable String name, HttpServletResponse response) {
+        response.setHeader("Access-Control-Allow-Origin", "*");
         return String.format("Hello %s! \n Use ACME's predictive stock recommendation and retire early. \n", name);
     }
 
     @GetMapping("/service/quote")
-    public String sayHello() {
+    @ResponseBody
+    public String sayHello(HttpServletResponse response) {
+        response.setHeader("Access-Control-Allow-Origin", "*");
         return String.format("Today's top stock: PVTL");
     }
 
